2411d9f6585120bfe29ab114ef65f607a5858710
AurelienGttn <aurelien.guitton@hotmail.com>
2018-11-08 15:06:51 +0100

    Auto map creation + ClickToMove

---
+++ SurvivalAI/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour {
+
+    private NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.destination = hit.point;
+            }
+        }
+	}
+}
