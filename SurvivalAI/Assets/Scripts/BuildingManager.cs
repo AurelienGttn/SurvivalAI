9583cae668239f49fac8935e6c46e99dc03b679a
William Zerbato <william.zerbato@esme.fr>
2018-11-22 14:47:40 -0500

    MiniMap

---
+++ SurvivalAI/Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour {
+
+    public GameObject[] buildings;
+    private BuildingPlacement buildingPlacement;
+
+    private void Start()
+    {
+        buildingPlacement = GetComponent<BuildingPlacement>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        for (int i = 0; i < buildings.Length; i++)
+        {
+            buildingPlacement.SetItem(buildings[i]);
+        }
+    }
+}
