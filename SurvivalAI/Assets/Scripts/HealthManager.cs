5114c7be1d3a1d24874956a007d7d5b6b76b28c9
AurelienGttn <aurelien.guitton@hotmail.com>
2018-11-24 22:44:58 +0100

    Added HealthBar behaviour

--- SurvivalAI/Assets/Scripts/HealthManager.cs
+++ SurvivalAI/Assets/Scripts/HealthManager.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour {
 
     [SerializeField] private ParticleSystem deathAnimation;
     public int health = 100;
     public int currentHealth;
+    [SerializeField] private Slider healthBar;
     
 	void Start ()
-        {
+    {
         currentHealth = health;
+        healthBar.maxValue = health;
 	}
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        healthBar.value = currentHealth;
         if (currentHealth <= 0)
         {
             if (gameObject.GetComponent<Renderer>().enabled)
